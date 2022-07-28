3520adc25dab7e5037172352a615a4fb3b40e706
nadialvy <nadia_lovely_29rpl@student.smktelkom-mlg.sch.id>
2022-07-28 12:33:50 +0700

    Filter date

--- lib/app/modules/all_presensi/controllers/all_presensi_controller.dart
+++ lib/app/modules/all_presensi/controllers/all_presensi_controller.dart
@@ -3,14 +3,47 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
 class AllPresensiController extends GetxController {
+  DateTime? start;
+  DateTime end = DateTime.now();
   
   FirebaseAuth auth = FirebaseAuth.instance;
   FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-  Stream<QuerySnapshot<Map<String, dynamic>>> streamAllPresence() async* {
-    //masuk ke curr user
+  Future<QuerySnapshot<Map<String, dynamic>>> getAllPresence() async {
     String uid = auth.currentUser!.uid;
 
-    yield* firestore.collection("pegawai").doc(uid).collection("presence").orderBy("date", descending: true).snapshots();
+    print(start);
+    print(end);
+
+    if(start == null){
+      // get all presensi sampai saat ini
+      return await firestore
+        .collection("pegawai")
+        .doc(uid)
+        .collection("presence")
+        // .where("date", isGreaterThan: start)
+        .where("date", isLessThan: end.toIso8601String())
+        .orderBy("date", descending: true)
+        .get();
+    }
+    else {
+      // get start sampai end yang dipilih
+      return await firestore
+        .collection("pegawai")
+        .doc(uid)
+        .collection("presence")
+        .where("date", isGreaterThan: start!.toIso8601String())
+        .where("date", isLessThan: end.add(const Duration(days: 1)).toIso8601String())
+        .orderBy("date", descending: true)
+        .get();
+    }
+
+  }
+
+  void pickDate(DateTime pickStart, DateTime pickEnd){
+    start = pickStart;
+    end = pickEnd;
+    update(); //mirip set state
+    Get.back();
   }
 }
--- lib/app/modules/all_presensi/views/all_presensi_view.dart
+++ lib/app/modules/all_presensi/views/all_presensi_view.dart
@@ -5,6 +5,7 @@ import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:presence_app/app/constant/colors.dart';
 import 'package:presence_app/app/routes/app_pages.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 
 import '../controllers/all_presensi_controller.dart';
 
@@ -17,121 +18,134 @@ class AllPresensiView extends GetView<AllPresensiController> {
         title: Text('All Presensi'),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(15),
-            child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15)
-              ),
-              elevation: 5,
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: 'Search...',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15)
+      body: GetBuilder<AllPresensiController>(
+        builder: (c) => FutureBuilder <QuerySnapshot<Map<String, dynamic>>>(
+          future: controller.getAllPresence(),
+          builder: (context, snapshot) {
+            if(snapshot.connectionState == ConnectionState.waiting){
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if(snapshot.data!.docs.isEmpty){
+                return Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.grey[200],
                   ),
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: StreamBuilder <QuerySnapshot<Map<String, dynamic>>>(
-              stream: controller.streamAllPresence(),
-              builder: (context, snapshot) {
-                if(snapshot.connectionState == ConnectionState.waiting){
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-                if(snapshot.data!.docs.isEmpty){
-                    return Container(
-                      padding: const EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: Colors.grey[200],
-                      ),
-                      child: const Text(
-                        "Belum ada history presensi!",
-                        style: TextStyle(
-                          color: mainRed,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    );
-                  }
-                return ListView.builder(
-                  padding: const EdgeInsets.only(left: 20, right: 20),
-                  itemCount: snapshot.data!.docs.length,
-                  itemBuilder: (context, index) {
-                    Map<String, dynamic> allPresence = snapshot.data!.docs[index].data();
-                    
-                      return Padding(
-                        padding: const EdgeInsets.only(bottom: 15),
-                        child: Material(
-                          color: Colors.grey[200],
-                          borderRadius: BorderRadius.circular(10),
-                          child: InkWell(
-                            onTap: () => Get.toNamed(Routes.DETAIL_PRESENSI, arguments: allPresence),
+                  child: const Text(
+                    "Belum ada history presensi!",
+                    style: TextStyle(
+                      color: mainRed,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              }
+            return ListView.builder(
+              padding: const EdgeInsets.all(20),
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                Map<String, dynamic> allPresence = snapshot.data!.docs[index].data();
+                
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 15),
+                    child: Material(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(10),
+                      child: InkWell(
+                        onTap: () => Get.toNamed(Routes.DETAIL_PRESENSI, arguments: allPresence),
+                        borderRadius: BorderRadius.circular(10),
+                        child: Container(
+                          padding: const EdgeInsets.all(20),
+                          decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(10),
-                            child: Container(
-                              padding: const EdgeInsets.all(20),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                 children: [
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      const Text(
-                                        'Masuk',
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.bold
-                                        ),
-                                      ),
-                                      Text(
-                                        DateFormat.yMMMEd().format(DateTime.parse(allPresence['date'])),
-                                        style: const TextStyle(
-                                          fontWeight: FontWeight.bold
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  Text(
-                                    '${allPresence['masuk']?['date'] == null 
-                                    ? "-"
-                                    : DateFormat.jms().format(DateTime.parse(allPresence['masuk']['date'])) } '
-                                  ),
-                                  const SizedBox(height: 10,),
                                   const Text(
-                                    'Keluar',
+                                    'Masuk',
                                     style: TextStyle(
                                       fontWeight: FontWeight.bold
                                     ),
                                   ),
                                   Text(
-                                    '${allPresence['keluar']?['date'] == null 
-                                    ? "-"
-                                    : DateFormat.jms().format(DateTime.parse(allPresence['keluar']['date'])) } '
+                                    DateFormat.yMMMEd().format(DateTime.parse(allPresence['date'])),
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.bold
+                                    ),
                                   ),
                                 ],
                               ),
-                            ),
+                              Text(
+                                '${allPresence['masuk']?['date'] == null 
+                                ? "-"
+                                : DateFormat.jms().format(DateTime.parse(allPresence['masuk']['date'])) } '
+                              ),
+                              const SizedBox(height: 10,),
+                              const Text(
+                                'Keluar',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold
+                                ),
+                              ),
+                              Text(
+                                '${allPresence['keluar']?['date'] == null 
+                                ? "-"
+                                : DateFormat.jms().format(DateTime.parse(allPresence['keluar']['date'])) } '
+                              ),
+                            ],
                           ),
                         ),
-                      );
+                      ),
+                    ),
+                  );
+              },
+            );
+          }
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Get.dialog(
+            Dialog(
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                height: 400,
+                child: SfDateRangePicker(
+                  todayHighlightColor: mainRed,
+                  selectionColor: mainRed,
+                  rangeSelectionColor: mainGrey,
+                  startRangeSelectionColor: mainRed,
+                  endRangeSelectionColor: mainRed,
+
+                  showActionButtons: true,
+                  onCancel: () => Get.back(),
+                  onSubmit: (obj) {
+                    if(obj != null){
+                      if((obj as PickerDateRange).endDate != null){
+                        controller.pickDate(obj.startDate!, obj.endDate!);
+                      }
+                    }
                   },
-                );
-              }
+
+                  monthViewSettings: const DateRangePickerMonthViewSettings(firstDayOfWeek: 1),
+                  selectionMode: DateRangePickerSelectionMode.range,
+                ),
+              ),
             ),
-          ),
-        ],
-      )
+          );
+        },
+        child: Icon(Icons.format_list_bulleted_rounded),
+        backgroundColor: mainRed,
+      ),
     );
   }
 }
